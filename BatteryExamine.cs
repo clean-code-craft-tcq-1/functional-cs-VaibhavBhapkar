353ca89033cff4216fc27454f392e84afdbb7c5d
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-03 18:57:26 +0530

    Refactoring changes with limits

--- BatteryExamine.cs
+++ BatteryExamine.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+
 public class BatteryExamine
 {
     private readonly IBatteryLimits batteryLimits;
@@ -20,6 +21,7 @@ public class BatteryExamine
     {
         if (batteryTemperature < batteryLimits.minTemperature || batteryTemperature > batteryLimits.maxTemperature)
         {
+            CompareTemperatureWithLimits(batteryTemperature);
             DisplayMessage("Temperature");
             return false;
         }
@@ -29,6 +31,7 @@ public class BatteryExamine
     {
         if (stateOfCharge < batteryLimits.minStateOfCharge || stateOfCharge > batteryLimits.maxStateOfCharge)
         {
+            CompareStateOfChargeWithLimits(stateOfCharge);
             DisplayMessage("State of Charge");
             return false;
         }
@@ -38,14 +41,46 @@ public class BatteryExamine
     {
         if (chargeRate > batteryLimits.minChargeRate)
         {
+            DisplayMaximumLimit("Charge Rate", batteryLimits.minChargeRate);
             DisplayMessage("Charge Rate");
             return false;
         }
         return true;
     }
+    public void CompareTemperatureWithLimits(float batteryTemperature)
+    {
+        if(batteryTemperature < batteryLimits.minTemperature)
+        {
+            DisplayMinimumLimit("Temperature", batteryLimits.minTemperature);
+        }
+        else
+        {
+            DisplayMaximumLimit("Temperature", batteryLimits.maxTemperature);
+        }
+    }
+    public void CompareStateOfChargeWithLimits(float stateOfCharge)
+    {
+        if (stateOfCharge < batteryLimits.minStateOfCharge)
+        {
+            DisplayMinimumLimit("State Of Charge", batteryLimits.minStateOfCharge);
+        }
+        else
+        {
+            DisplayMaximumLimit("State Of Charge", batteryLimits.maxStateOfCharge);
+        }
+    }
+   
     static void DisplayMessage(string batteryParameter)
     {
         Console.WriteLine("{0} is out of range!", batteryParameter);
+    }
+    static void DisplayMinimumLimit(string batteryParameter,float batteryParameterLimit)
+    {
+        Console.WriteLine("{0} value is less than minimun limit of {1}!", batteryParameter,batteryParameterLimit);
+    }
+    static void DisplayMaximumLimit(string batteryParameter,float batteryParameterLimit)
+    {
+        Console.WriteLine("{0} value is more than maximum limit of {1}!", batteryParameter,batteryParameterLimit);
     }
 }
 
