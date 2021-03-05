72e238d68dd4582f0ab7cc12775bc6de59009b78
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-05 22:13:50 +0530

    Changes made after review comments

--- checker.cs
+++ checker.cs
@@ -3,33 +3,18 @@ using System.Diagnostics;
 
 class Checker
 {
-    static void ExpectOkBattery(bool expression)
-    {
-        if (!expression)
-        {
-            Console.WriteLine("Expected true, but got false");
-        }
-    }
-    static void ExpectFaultyBattery(bool expression)
-    {
-        if (expression)
-        {
-            Console.WriteLine("Expected false, but got true");
-        }
-    }
-    static int Main()
+   static int Main()
     {
         IBatteryLimits iBatteryLimits = new BatteryLimits();
         BatteryExamine batteryExamine = new BatteryExamine(iBatteryLimits);
         Console.WriteLine("-- Tests which are returning correct results with true limits --");
-        ExpectOkBattery(batteryExamine.BatteryIsOk(25, 70, 0.7f));
-        ExpectFaultyBattery(batteryExamine.BatteryIsOk(50, 85, 0.3f));
-        Console.WriteLine("All Ok !!!");
+        batteryExamine.BatteryIsOk(25, 70, 0.7f);
+        batteryExamine.BatteryIsOk(50, 85, 0.3f);
         Console.WriteLine("-- Tests which are returning false results with fake limits --");
         IBatteryLimits iBatteryFakeLimits = new FakeBatteryLimits();
         BatteryExamine fakeBatteryExamine = new BatteryExamine(iBatteryFakeLimits);
-        ExpectOkBattery(fakeBatteryExamine.BatteryIsOk(25, 70, 0.7f));
-        ExpectFaultyBattery(fakeBatteryExamine.BatteryIsOk(50, 85, 0.3f));
+        fakeBatteryExamine.BatteryIsOk(25, 70, 0.7f);
+        fakeBatteryExamine.BatteryIsOk(50, 85, 0.3f);
         return 0;
-    }
+    } 
 }
