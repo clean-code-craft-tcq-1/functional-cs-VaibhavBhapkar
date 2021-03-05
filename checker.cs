884eef72cfb4c0f3b051591b1d92766a38091854
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-05 22:56:06 +0530

    Adding capability to extend features

--- checker.cs
+++ checker.cs
@@ -8,13 +8,13 @@ class Checker
         IBatteryLimits iBatteryLimits = new BatteryLimits();
         BatteryExamine batteryExamine = new BatteryExamine(iBatteryLimits);
         Console.WriteLine("-- Tests which are returning correct results with true limits --");
-        batteryExamine.BatteryIsOk(25, 70, 0.7f);
-        batteryExamine.BatteryIsOk(50, 85, 0.3f);
+        batteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f));
+        batteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f));
         Console.WriteLine("-- Tests which are returning false results with fake limits --");
         IBatteryLimits iBatteryFakeLimits = new FakeBatteryLimits();
         BatteryExamine fakeBatteryExamine = new BatteryExamine(iBatteryFakeLimits);
-        fakeBatteryExamine.BatteryIsOk(25, 70, 0.7f);
-        fakeBatteryExamine.BatteryIsOk(50, 85, 0.3f);
+        fakeBatteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f));
+        fakeBatteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f));
         return 0;
-    } 
+    }
 }
