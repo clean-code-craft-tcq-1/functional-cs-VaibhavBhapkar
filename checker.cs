5ccd28641ce19cce163495b64d5d499fa74026b7
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-03-20 10:50:37 +0530

    Making functions testable

--- checker.cs
+++ checker.cs
@@ -3,18 +3,32 @@ using System.Diagnostics;
 
 class Checker
 {
-   static int Main()
+    static void ExpectTrue(bool expression)
+    {
+        if (!expression)
+        {
+            Console.WriteLine("Battery condition expected normal, but got faulty");
+        }
+    }
+    static void ExpectFalse(bool expression)
+    {
+        if (expression)
+        {
+            Console.WriteLine("Battery condition expected faulty, but got normal");
+        }
+    }
+    static int Main()
     {
         IBatteryLimits iBatteryLimits = new BatteryLimits();
         BatteryExamine batteryExamine = new BatteryExamine(iBatteryLimits);
         Console.WriteLine("-- Tests which are returning correct results with true limits --");
-        batteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f));
-        batteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f));
+        ExpectTrue(batteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f)));
+        ExpectFalse(batteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f)));
         Console.WriteLine("-- Tests which are returning false results with fake limits --");
         IBatteryLimits iBatteryFakeLimits = new FakeBatteryLimits();
         BatteryExamine fakeBatteryExamine = new BatteryExamine(iBatteryFakeLimits);
-        fakeBatteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f));
-        fakeBatteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f));
+        ExpectTrue(fakeBatteryExamine.BatteryIsOk(new BatteryFactors(25, 70, 0.7f)));
+        ExpectFalse(fakeBatteryExamine.BatteryIsOk(new BatteryFactors(50, 85, 0.3f)));
         return 0;
     }
 }
